73c18bff4acf288126c28baf50c788b17a41a92c
Anantha debnath <88941537+Ananthadeb1@users.noreply.github.com>
2022-04-18 03:23:40 +0600

    data stored section half done

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import {addToDb, getStoredCart} from '../../utilities/fakedb'
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product'
 import './Shop.css'
@@ -10,9 +11,21 @@ const Shop = () => {
         .then(res=> res.json())
         .then(data=> setproducts(data))
 },[]);
+
+useEffect(()=>{
+   const storedCart = getStoredCart();
+   for(const id in storedCart ){
+    const addedProduct = products.find(product=>product.id ===id);
+        console.log(addedProduct);
+}
+},[])
+
+
+
 const handleAddToCart =(product)=>{
     const newCart = [...cart,product];
     setcart(newCart)
+    addToDb(product.id);
 }
     return (
         <div className='shop-container'>
@@ -25,7 +38,7 @@ const handleAddToCart =(product)=>{
                 };
             </div>
             <div className="cart-container">
-                <div className="cart-text">
+                <div>
                     <Cart cart={cart}></Cart>
                 </div>
             </div>
